2eec5c8087026913f7647266c8fcc9b1abf715f8
lnmunhoz <ln.munhoz@gmail.com>
2016-05-17 19:22:10 -0300

    Fix startup seed

--- src/server/fixtures/admin_account.js
+++ src/server/fixtures/admin_account.js
@@ -1,26 +1,25 @@
-const ROLES = ['god', 'admin', 'editor', 'viewer'];
+const ROLES = ['admin', 'editor', 'viewer'];
+
 Meteor.startup(function() {
-  //
-  //  Add root
-  //
-  if (!Meteor.users.findOne({
-      'emails.address': 'admin@admin.com'
-    })) {
-    let userId = Accounts.createUser({
-      email: 'admin@admin.com',
-      password: 'q1w2e3'
-    });
-    Roles.addUsersToRoles(userId, 'admin');
+  // Seed admin for development
+  if (process.env.NODE_ENV === 'development') {
+    if (!Meteor.users.findOne({
+        'emails.address': 'admin@admin.com'
+      })) {
+      let userId = Accounts.createUser({
+        email: 'admin@admin.com',
+        password: 'q1w2e3'
+      });
+      Roles.addUsersToRoles(userId, 'admin');
+    }
   }
 
-  //
-  //  Preserve only determined ROLES, add them if do not exist
-  //
-    Meteor.roles.remove({
-      name: {$nin: ROLES}
-    });  
+  // Seed Roles
+  Meteor.roles.remove({
+    name: { $nin: ROLES }
+  });
   _.each(ROLES, function(role, index) {
-    let existingRole = Meteor.roles.findOne({name: role});
+    let existingRole = Meteor.roles.findOne({ name: role });
     Meteor.roles.upsert({
       name: role
     }, {
