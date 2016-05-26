61f599566f148f1a8566a6e22b6189541dc1a9bd
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-05-26 17:30:34 -0300

    Fixed index name from settings

--- src/imports/api/elastic_search/index.js
+++ src/imports/api/elastic_search/index.js
@@ -1,8 +1,10 @@
 import { ElasticSearchIndexAPI } from './lib/elastic_search_index.js';
 import esClient from './es_client.js';
 
+let index = Meteor.settings.public.elasticSearch.index;
+
 let elasticSearchIndex = new ElasticSearchIndexAPI({
-	index: 'tdb-development',
+	index: index,
 	esClient: esClient
 });
 
